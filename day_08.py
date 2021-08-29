3046499990eaa5e3ad74eb645a0a9502559a65f5
Danielalara586 <84215756+Danielalara586@users.noreply.github.com>
2021-08-29 13:23:08 -0500

    Added day 8

---
+++ day_08.py
@@ -0,0 +1,15 @@
+# Day 8: Cylinder's Volumme
+
+import math
+
+def cylinder_volume(h: int, r: int) -> int:
+    return math.pi * (r**2) * h
+
+def run():
+    height = int(input("Please enter the height: "))
+    radius = int(input("Please enter the radius: "))
+    result = round(cylinder_volume(height, radius), 2)
+    print(f"The cylinder's volume is: {result} m^3.")
+
+if __name__ == "__main__":
+    run()
