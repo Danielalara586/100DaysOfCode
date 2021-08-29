94909b66b541e5c7e7b7d852d9f6bc05d118d217
Danielalara586 <84215756+Danielalara586@users.noreply.github.com>
2021-08-29 13:13:40 -0500

    Update day_08.py
    Grammar correction

--- day_08.py
+++ day_08.py
@@ -1,15 +1,15 @@
-# Day 8: Cilinder's Volumme
+# Day 8: Cylinder's Volumme
 
 import math
 
-def cilinder_volume(h: int, r: int) -> int:
+def cylinder_volume(h: int, r: int) -> int:
     return math.pi * (r**2) * h
 
 def run():
     height = int(input("Please enter the height: "))
     radius = int(input("Please enter the radius: "))
-    result = round(cilinder_volume(height, radius), 2)
-    print(f"The cilinder's volume is {result}m^3.")
+    result = round(cylinder_volume(height, radius), 2)
+    print(f"The cylinder's volume is {result}m^3.")
 
 if __name__ == "__main__":
     run()
