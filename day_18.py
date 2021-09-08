fbe9024c4cd3a1d9ce832e6b901a6ad20e37ddb9
Daniela Lara <laradni@icloud.com>
2021-09-08 09:35:03 -0500

    Added day 18

---
+++ day_18.py
@@ -0,0 +1,14 @@
+# Day 1: Memory Game! Part 1
+
+import random 
+
+def memory_game():
+    char = "#$%()/&"
+    return ''.join(random.choice(char)for i in range(4)) 
+
+def run():
+    chars = memory_game()
+    print(f"Random charactes: {chars}")
+
+if __name__ == "__main__":
+    run()
