254c432cb60a09eab58d22469de32b9d34a811d8
Daniela Lara <laradni@icloud.com>
2021-09-13 19:39:08 -0500

    Added day 23

---
+++ day_23.py
@@ -0,0 +1,41 @@
+# Day 23: Upgrading the savings calculator
+
+import os
+
+os.system("clear")
+print("Welcome to savings calculator! \nWanna know how much money you'll save with a 4% interest rate?")
+amounts: float = []
+month_g: int = []
+saved_money = {}
+
+def upgraded_calculator(amount: float, months: int):
+    INTEREST_RATE: float = 0.04
+    saving: float = amount
+    for i in range (months):
+        saving = saving + (saving * (INTEREST_RATE))
+
+    return saving
+
+def savings():
+    x = int(input("How many amounts do you want to add?: "))
+    for i in range(x):
+        amount = float(input("Please enter an amount: "))
+        months = int(input("Please enter the months: "))
+        total = round(upgraded_calculator(amount, months), 2)
+        amounts.append(total)
+        month_g.append(str(months))
+        
+        saved_money[amounts[i]] = month_g[i]
+
+
+    return saved_money
+
+
+def run():
+    total = savings()
+    for key in total:
+        print(f"You'll save ${key} in {total[key]} months.")
+
+
+if __name__ == "__main__":
+    run()
