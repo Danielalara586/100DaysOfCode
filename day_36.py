9dffb890e122c211adfd8ef77714832df2455d9a
Daniela Lara <laradni@icloud.com>
2021-09-26 21:03:02 -0500

    Added day 36

---
+++ day_36.py
@@ -0,0 +1,37 @@
+# Day 36: Tic-Tac-Toe Board
+
+import pygame, sys
+
+# Colors
+BLACK = (0, 0, 0)
+BLUE = (28, 170, 156)
+
+pygame.init()
+window = pygame.display.set_mode((600, 600))
+pygame.display.set_caption('Tic Tac Toe')
+window.fill(BLACK)
+
+
+def board():
+    # Horizontal Lines
+    pygame.draw.line(window, BLUE, (0, 200), (600, 200), 15)
+    pygame.draw.line(window, BLUE, (0, 400), (600, 400), 15)
+
+    # Vertical Lines
+    pygame.draw.line(window, BLUE, (200, 0), (200, 600), 15)
+    pygame.draw.line(window, BLUE, (400, 0), (400, 600), 15)
+
+
+board()
+
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+
+    pygame.display.update()
+
+
+
+
