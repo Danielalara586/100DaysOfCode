ec35b5d4eb950d4ea75ab72193fe8e115d5d1c5a
Daniela Lara <laradni@icloud.com>
2021-11-11 15:23:36 -0600

    Added day 56

---
+++ day_56.py
@@ -0,0 +1,21 @@
+# Day 56: Port Scanner
+
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.settimeout(5)
+
+host = input("Please enter the IP you want to scan: ")
+port = int(input("Please enter the port: "))
+
+
+def port_scanner(port):
+    if s.connect_ex((host, port)):
+        print("The port is closed")
+    else:
+        print("The port is open")
+
+
+if __name__ == "__main__":
+    port_scanner(port)
+
