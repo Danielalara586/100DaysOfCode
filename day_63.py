d7b35868b682fc9fea1068a14d4829acfd8ce9cc
Daniela Lara <laradni@icloud.com>
2022-02-16 12:38:07 -0600

    Add game images

---
+++ day_63.py
@@ -0,0 +1,24 @@
+# Day 63: Flappy Bird Pt. 1
+
+import pygame
+from pygame.locals import *
+
+pygame.init();
+
+screen_width = 864
+screen_height = 936
+
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption('Flappy Bird')
+
+# Load images
+bg = pygame.image.load('')
+
+run = True
+
+while run:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+pygame.quit()
